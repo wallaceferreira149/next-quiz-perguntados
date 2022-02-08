12fbfb3fa4dd344cd3070afbef11d25c10257432
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-02-08 06:58:10 -0400

    feat: create component timer
    timer component use lib react countdown circle timer and define the time
    to response the question

---
+++ src/components/Timer.tsx
@@ -0,0 +1,24 @@
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import styles from '../styles/Timer.module.scss'
+
+type TimerProps = {
+  duration: number
+  timeOut: () => void
+}
+
+export default function Timer(props: TimerProps) {
+  return (
+    <div className={styles.timer}>
+      <CountdownCircleTimer
+        duration={props.duration}
+        size={120}
+        isPlaying
+        onComplete={props.timeOut}
+        colors={['#BCE596', '#F7B801', '#ED827A', '#ED827A']}
+        colorsTime={[10, 5, 3, 0]}
+      >
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    </div>
+  )
+}
