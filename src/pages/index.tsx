e8a19a8137107d76055bab4ae372404fcdd82b6f
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-02-08 23:10:27 -0400

    feat: finish index page
    index hold all logic to fetch data from database, count correct answer,
    navigate thorow the question, and so on

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,89 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Quiz } from '../components/Quiz'
+import QuizModel from '../model/quiz'
+
+const BASE_URL = 'http://localhost:3000/api'
 
 const Home: NextPage = () => {
-  return <h1>Página Inicial</h1>
+  const router = useRouter()
+  const [questionsId, setQuestionsId] = useState<number[]>([])
+  const [question, setQuestion] = useState<QuizModel>()
+  const [correctAnswers, setCorrectAnswers] = useState<number>(0)
+
+  async function fetchQuestionsId() {
+    try {
+      const resp = await fetch(`${BASE_URL}/quiz`)
+      const ids = await resp.json()
+      console.log(ids)
+      setQuestionsId(ids)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async function fetchQuestion(id: number) {
+    try {
+      const resp = await fetch(`${BASE_URL}/questions/${id}`)
+      const questionJSON = await resp.json()
+      const newQuestion = QuizModel.fromObject(questionJSON)
+      setQuestion(newQuestion)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuestionsId()
+  }, [])
+
+  useEffect(() => {
+    questionsId.length > 0 && fetchQuestion(questionsId[0])
+  }, [questionsId])
+
+  function questionAnswered(question: QuizModel) {
+    setQuestion(question)
+    const isCorrectAnswer = question.correct
+    setCorrectAnswers(correctAnswers + (isCorrectAnswer ? 1 : 0))
+  }
+
+  function nextQuestionId() {
+    if (question) {
+      const nextId = questionsId.indexOf(question.id) + 1
+      return questionsId[nextId]
+    }
+  }
+
+  function goNextStep() {
+    const nextId = nextQuestionId()
+    nextId ? goNextQuestion(nextId) : finale()
+  }
+
+  function goNextQuestion(nextId: number) {
+    fetchQuestion(nextId)
+  }
+
+  function finale() {
+    router.push({
+      pathname: '/result',
+      query: {
+        total: questionsId.length,
+        correctAnswers: correctAnswers,
+      },
+    })
+  }
+
+  return question ? (
+    <Quiz
+      question={question}
+      isLast={nextQuestionId() === undefined}
+      questionAnswered={questionAnswered}
+      goNextStep={goNextStep}
+    />
+  ) : (
+    <h1>Loading...</h1>
+  )
 }
 
 export default Home
