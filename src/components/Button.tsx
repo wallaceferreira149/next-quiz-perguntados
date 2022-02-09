f7a8c927388592375a58044bda0ec198edb4bdad
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-02-08 20:57:22 -0400

    feat: add button component
    generic button to be used  in answer and general proposal

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import styles from '../styles/Button.module.scss'
+
+type ButtonProps = {
+  text: string
+  href?: string
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+export default function Button(props: ButtonProps) {
+  return props.href ? (
+    <Link href={props.href}>
+      <button className={styles.button} onClick={props.onClick}>
+        {props.text}
+      </button>
+    </Link>
+  ) : (
+    <button className={styles.button} onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
